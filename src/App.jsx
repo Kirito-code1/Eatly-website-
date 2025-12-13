62ec5545441723f752042700aeb7995f830e3e6a
Kirito-code1 <yigitalievshahzod821@gmail.com>
2025-12-13 21:30:45 +0500

    final changes witout response

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import MainPage from './mainPage/Main_page';
 import AdvertPage from './mainPage/advert_page';
 import RestPage from './mainPage/Rest_page';
+import MessagePart from './mainPage/MessagePart';
 import FooterPage from './mainPage/Footer_page';
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
 			<AdvertPage/>
 		<RestPage/>
 		<MessagePart/>
+		<FooterPage/>
 		</>
 	)
 }
