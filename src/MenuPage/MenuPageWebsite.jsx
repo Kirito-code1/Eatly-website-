c68d3e37c507f56b7dd3749613bba6e58d6efb5a
Kirito-code1 <yigitalievshahzod821@gmail.com>
2025-12-20 21:46:01 +0500

    changes

--- src/MenuPage/MenuPageWebsite.jsx
+++ src/MenuPage/MenuPageWebsite.jsx
@@ -1,25 +1,18 @@
-import { Link } from "react-router-dom";
-import Footer from '../mainPage/individualFooter'
+import { Link } from "react-router-dom";    
 import SecondDishesCard from "./SecondDishesCard";
-import Questions from "./questions";
+import Footer from "../mainPage/individualFooter";
+import MenuPageTopSide from "./MenuPageTopSide";
+import Questions from "./Questions";
+import '../css/blocks/main_page.css';
 
 
 export default function MenuPageWebsite(){
     return (
 			<>
+            <MenuPageTopSide/>
 				<SecondDishesCard />
 				<Questions/>
 				<Footer />
 			</>
 		)
-import Footer from "../mainPage/individualFooter";
-import MenuPageTopSide from "./MenuPageTopSide";
-import '../css/blocks/main_page.css'
-
-export default function MenuPageWebsite(){
-    return(
-        <>
-        <MenuPageTopSide/>
-        </>
-    )
-}
+    }
